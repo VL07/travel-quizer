637dd65292aa26b0578c8f14f93d8796fa423b06
VL07 <79533745+VL07@users.noreply.github.com>
2021-06-08 12:45:29 +0200

    Added signup
    but no styling

--- travel-quizer/src/App.js
+++ travel-quizer/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { AuthProvider } from "./contexts/AuthContext"
 import Help from "./pages/Help";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
@@ -6,20 +7,22 @@ import Signup from "./pages/Signup";
 function App() {
   return (
     <div className="App">
-      {/* Routes */}
-      <Router>
-        <Switch>
-          {/* Home route */}
-          <Route exact path="/" component={Home} />
+      <AuthProvider>
+        {/* Routes */}
+        <Router>
+          <Switch>
+            {/* Home route */}
+            <Route exact path="/" component={Home} />
 
-          {/* Help route */}
-          <Route exact path="/help" component={Help} />
+            {/* Help route */}
+            <Route exact path="/help" component={Help} />
 
-          {/* AUTH */}
-          {/* Signup route */}
-          <Route exact path="/signup" component={Signup} />
-        </Switch>
-      </Router>
+            {/* AUTH */}
+            {/* Signup route */}
+            <Route exact path="/signup" component={Signup} />
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
--- travel-quizer/src/pages/Signup.js
+++ travel-quizer/src/pages/Signup.js
@@ -1,21 +1,59 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
+import { useHistory } from 'react-router'
+import { useAuth } from "../contexts/AuthContext"
 
 export default function Signup() {
+    // refs
     const emailRef = useRef()
     const passwordRef = useRef()
     const repeatPasswordRef = useRef()
 
+    // states
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    // Use auth thing
+    const { signupUser } = useAuth()
+
+    // history to redirect
+    const history = useHistory()
+
+
+
     function onSubmit(e) {
+        // prevents reload
         e.preventDefault()
+
+        // make the user unable to submit two times
+        setLoading(true)
+
+        // Chacks is passwords dosn't match
+        if (passwordRef.current.value !== repeatPasswordRef.current.value) {
+            setLoading(false)
+            return setError("Password dosn't match")
+        }
+
+        // Trys to create user
+        try {
+            setError("")
+            signupUser(emailRef.current.value, passwordRef.current.value)
+
+            history.push("/")
+        } catch {
+            setError("Unable to create user")
+        }
+        setLoading(false)
     }
 
     return (
         <div>
+            {/* If error display it */}
+            {error && <p>{error}</p>}
             <form onSubmit={onSubmit} >
                 <input type="email" ref={emailRef} placeholder="Email" name="email" />
                 <input type="password" ref={passwordRef} placeholder="Password" name="password" />
                 <input type="password" ref={repeatPasswordRef} placeholder="Confirm password" name="repeat-password" />
-                <input type="submit" name="submit" />
+                <input type="submit" name="submit" value="Signup" disabled={loading} />
             </form>
         </div>
     )
