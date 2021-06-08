22b1602fb97e2cd83ada6621d3ea627161e22b99
VL07 <79533745+VL07@users.noreply.github.com>
2021-06-08 10:24:10 +0200

    Added react router and a home page

--- travel-quizer/src/App.js
+++ travel-quizer/src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      Hello
+      {/* Routes */}
+      <Router>
+        <Switch>
+          {/* Home route */}
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
