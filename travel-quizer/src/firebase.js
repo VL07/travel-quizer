9a080713b0792cbfcc3acb47280ca7902ba1793c
VL07 <79533745+VL07@users.noreply.github.com>
2021-06-08 16:10:08 +0200

    Added a way to get user data from dashboard

--- travel-quizer/src/firebase.js
+++ travel-quizer/src/firebase.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app"
 import "firebase/auth"
+import "firebase/firestore"
 
 
 const app = firebase.initializeApp({
@@ -12,4 +13,5 @@ const app = firebase.initializeApp({
 })
 
 export const auth = app.auth()
+export const db = firebase.firestore()
 export default app
--- travel-quizer/src/pages/Dashboard.js
+++ travel-quizer/src/pages/Dashboard.js
@@ -1,14 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import LogoutButton from '../components/LogoutButton'
 import { useAuth } from '../contexts/AuthContext'
+import { db } from '../firebase'
 
 export default function Dashboard() {
     const { currentUser } = useAuth() 
-    console.log(currentUser)
+    
+    // sets the states
+    const [userData, setUserData] = useState({username: "Coming soon", score: 1})
+
+    
+
+
+    useEffect(() => {
+        // gets the users document from firestore
+        const usersDbRef = db.collection("users").doc(currentUser.uid)
+
+        // gets the data
+        usersDbRef.get().then((doc) => {
+            if (doc.exists) {
+                console.log("Document data:", doc.data());
+                setUserData(doc.data())
+            } else {
+                // Data dosn't exist; setting data
+                console.log("No such document!");
+
+                usersDbRef.set({username: "Coming soon", score: 1}).then(() => {
+                    console.log("Successfully wrote data")
+                }).catch((error) => {
+                    console.error("Error writing document: ", error)
+                })
+            }
+        }).catch((error) => {
+            console.log("Error getting document:", error);
+        })
+    }, [currentUser.uid])
+    
+
     return (
         <div>
             <p>{currentUser.email}</p>
             <LogoutButton>Logout</LogoutButton>
+            <p>Score: {userData.score}</p>
         </div>
     )
 }
