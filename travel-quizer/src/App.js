42ffdc61f8e7273e19bc0eb204dab6432fe41e91
VL07 <79533745+VL07@users.noreply.github.com>
2021-06-08 10:32:41 +0200

    Added help page
    More help coming soon

--- travel-quizer/src/App.js
+++ travel-quizer/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Help from "./pages/Help";
 import Home from "./pages/Home";
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
         <Switch>
           {/* Home route */}
           <Route exact path="/" component={Home} />
+
+          {/* Help route */}
+          <Route exact path="/help" component={Help} />
         </Switch>
       </Router>
     </div>
---
+++ travel-quizer/src/pages/Help.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Help() {
+    return (
+        <div>
+            <h1>More help coming soon</h1>
+        </div>
+    )
+}
