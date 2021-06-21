05e35cffcc1b3c6d451e27fc981a4e47b297bd4e
VL07 <79533745+VL07@users.noreply.github.com>
2021-06-21 09:46:51 +0200

    Quiz page now reads from the db

--- travel-quizer/src/pages/Quiz.js
+++ travel-quizer/src/pages/Quiz.js
@@ -1,16 +1,59 @@
-import React from 'react'
-import { useParams } from 'react-router'
+import React, { useEffect, useState } from 'react'
+import { useHistory, useParams } from 'react-router'
 import { withScriptjs, withGoogleMap } from 'react-google-maps';
 import GoogleMapComp from '../components/GoogleMapComp';
+import { db } from "../firebase"
 
 
 export default function Quiz() {
     // get url parameters
     const { id } = useParams()
+    console.log(id)
+
+    // states
+    const [quizData, setQuizData] = useState()
+
+    const history = useHistory()
+
+    useEffect(() => {
+        // gets the quizs document from firestore
+        const quizesDbRef = db.collection("quizes").doc(id)
+
+        // gets the data
+        quizesDbRef.get().then((doc) => {
+            if (doc.exists) {
+                setQuizData(doc.data())
+
+               
+                
+            } else {
+                console.log("Quiz id was invalid; redirecting...")
+                history.push("/dashboard/")
+            }
+        }).catch((error) => {
+            console.log("Error getting document:", error);
+            history.push("/dashboard")
+        })
+    }, [id, history])
+
+    useEffect(() => {
+        if (!quizData) { return }
+         // geting creators doc
+         const usersDbRef = db.collection("users").doc(quizData.creator.id)
+         // getting data
+         usersDbRef.get().then((usersDoc) => {
+             console.log(usersDoc.data())
+         })
+    }, [quizData])
 
     // the wraped map
     const WrapedMap = withScriptjs(withGoogleMap(GoogleMapComp))
 
+
+    console.log(quizData)
+    
+
+    
     return (
         <div>
             quiz {id}
